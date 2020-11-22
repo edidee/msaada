bfe58fd7868063b0394b373b71083fbcb812d431
edidee <etokedidionge@gmail.com>
2020-11-22 21:08:43 +0100

    hire me page

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ function hireForm(){
     let hireMeForm = document.getElementById('hire-me-form');
     hireMeForm.style.display = 'block'
 }
-
+document.addEventListener('click', function(e){
+    var clik = e.target.id;
+    if (clik == 'hire-me-form'){
+        var stoop = document.getElementById('hire-me-form')
+        stoop.style.display='none'
+    }
+})
 let hireMe = document.getElementsByClassName(hirebtn);
