6d265cf53d49b08750b384e16fa44b6f49467a71
edidee <etokedidionge@gmail.com>
2020-11-23 10:26:19 +0100

    add forms

--- app.js
+++ app.js
@@ -5,10 +5,34 @@ function hireForm(){
     hireMeForm.style.display = 'block'
 }
 document.addEventListener('click', function(e){
-    var clik = e.target.id;
+    let clik = e.target.id;
     if (clik == 'hire-me-form'){
-        var stoop = document.getElementById('hire-me-form')
+        let stoop = document.getElementById('hire-me-form')
         stoop.style.display='none'
     }
 })
-let hireMe = document.getElementsByClassName(hirebtn);
+
+
+function signinForm(){
+    let signForm = document.getElementById('signin-form');
+    signForm.style.display = 'block'
+}
+document.addEventListener('click', function(e){
+    let press = e.target.id;
+    if (press == 'signin-form'){
+        let stop = document.getElementById('signin-form')
+        stop.style.display='none'
+    }
+})
+
+function signupForm(){
+    let sign_upForm = document.getElementById('signup-form');
+    sign_upForm.style.display = 'block'
+}
+document.addEventListener('click', function(e){
+    let clickon = e.target.id;
+    if (clickon == 'signup-form'){
+        let close = document.getElementById('signup-form')
+       close.style.display='none'
+    }
+})
